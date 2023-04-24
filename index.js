c2b9bffbbfbc823b9eeaeacc9e84024b5cf1d429
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-24 19:09:46 +0200

    create title property in the obeject API and create validation for the title

--- index.js
+++ index.js
@@ -12,18 +12,21 @@ app.use(logger)
 let notes = [
   {
     id: 1,
+    title: 'HTML is easy',
     content: 'HTML is easy!',
     date: '2019-05-30T17:30:31.098Z',
     important: true
   },
   {
     id: 2,
+    title: 'Browser can execute only Javascript',
     content: 'Browser can execute only Javascript',
     date: '2019-05-30T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
+    title: 'GET and POST are the most important methods of HTTP protocol',
     content: 'GET and POST are the most important methods of HTTP protocol',
     date: '2019-05-30T19:20:14.298Z',
     important: true
@@ -65,7 +68,7 @@ app.delete('/api/notes/:id', (request, response) => {
 app.post('/api/notes', (request, response) => {
   const note = request.body
 
-  if (!note || !note.content) {
+  if (!note || !note.content || !note.title) {
     return response.status(400).json({
       error: 'note content is missing'
     })
@@ -79,6 +82,7 @@ app.post('/api/notes', (request, response) => {
   // Create a new note object
   const newNote = {
     id: Math.max(...ids) + 1,
+    title: note.title,
     content: note.content,
     important: typeof note.important !== 'undefined' ? note.important : false,
     date: new Date().toISOString()
