d9ad17e78bf8b54bfb71bbec8781dbcf5c0ba9c6
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-05-02 23:21:35 +0200

    create delete and post request in the reviews controller

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -42,4 +42,43 @@ usersRouter.put('/:id', (request, response, next) => {
     })
 })
 
+usersRouter.delete('/:id', async (request, response, next) => {
+  try {
+    const { id } = request.params
+    const result = await Review.findOneAndRemove({ _id: id })
+
+    if (result === null) {
+      next('CastError')
+      // return response.status(400).json({ error: 'Invalid review ID' })
+    }
+
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+usersRouter.post('/', async (request, response, next) => {
+  const review = request.body
+
+  if (!review || !review.content || !review.title) {
+    return response.status(400).json({
+      error: 'review content is missing'
+    })
+  }
+
+  const newReview = new Review({
+    title: review.title,
+    content: review.content,
+    important: typeof review.important !== 'undefined' ? review.important : false
+  })
+
+  try {
+    const savedReview = await newReview.save()
+    response.status(200).json(savedReview)
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = usersRouter
--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const logger = require('./middleware/loggerMiddleware')
 const notFound = require('./middleware/notFound')
 const handleErrors = require('./middleware/handleErrors')
 
-const Review = require('./models/Review')
-
 // Import controllers
 const usersRouter = require('./controllers/users')
 const reviewsRouter = require('./controllers/reviews')
@@ -47,89 +45,7 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-// app.get('/api/reviews', async (request, response) => {
-//   const reviews = await Review.find({})
-//   response.json(reviews)
-// })
-
-app.get('/api/reviews/:id', (request, response, next) => {
-  const { id } = request.params
-
-  Review.findById(id).then(review => {
-    if (review) {
-      console.log(review)
-      response.json(review)
-    } else {
-      // response.send('<h1>no hay</h1>');
-      response.status(404).end()
-    }
-  }).catch(error => {
-    // response.status(400).send({ error: 'bad formatted id' }).end()
-    // response.status(400).end()
-    next(error)
-  })
-})
-
-app.put('/api/reviews/:id', (request, response, next) => {
-  const { id } = request.params
-
-  const review = request.body
-
-  const newReviewInfo = {
-    title: review.title,
-    content: review.content,
-    important: review.important
-  }
-
-  Review.findByIdAndUpdate(id, newReviewInfo, { new: true })
-    .then(result => {
-      // Result is the old review
-      response.status(200).json(result)
-    }).catch(error => {
-      next(error)
-    })
-})
-
-app.delete('/api/reviews/:id', async (request, response, next) => {
-  try {
-    const { id } = request.params
-    const result = await Review.findOneAndRemove({ _id: id })
-
-    if (result === null) {
-      next('CastError')
-      // return response.status(400).json({ error: 'Invalid review ID' })
-    }
-
-    response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
-})
-
-app.post('/api/reviews', async (request, response, next) => {
-  const review = request.body
-
-  if (!review || !review.content || !review.title) {
-    return response.status(400).json({
-      error: 'review content is missing'
-    })
-  }
-
-  const newReview = new Review({
-    title: review.title,
-    content: review.content,
-    important: typeof review.important !== 'undefined' ? review.important : false
-  })
-
-  try {
-    const savedReview = await newReview.save()
-    response.status(200).json(savedReview)
-  } catch (error) {
-    next(error)
-  }
-})
-
-app.use('api/reviews', reviewsRouter)
+app.use('/api/reviews', reviewsRouter)
 
 app.use('api/users', usersRouter)
 
