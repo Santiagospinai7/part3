0ec773f39f0ded69a9cda175461e35329442495b
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-05-03 10:38:24 +0200

    install bcrypt to create a password hash

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 
@@ -5,10 +6,13 @@ usersRouter.post('/', async (request, response) => {
   const { body } = request
   const { username, name, password } = body
 
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
   const user = new User({
     username,
     name,
-    passwordHash: password
+    passwordHash
   })
 
   const savedUser = await user.save()
